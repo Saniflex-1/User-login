1af79ee7eea0b7e2ac2b554bdeadb33c90c653d7
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2024-08-08 13:19:11 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <!-- <link rel="stylesheet" href="./css/style.css"> -->
+     <style>
+        *{
+            margin: 0;
+            padding: 0;
+        }
+
+        .home{
+            height: 100vh; 
+            display:flex; 
+          align-items:center;
+          background: linear-gradient(to top,
+          rgba(255, 255, 255, 0.8),
+          rgba(255, 0, 0, 0.6),
+          rgba(255, 255, 255, 0.2)
+          );
+        }
+     </style>
+</head>
+<body>
+    
+    
+    <div class="home">
+        <p >don't have an account? <a href="register.php">regiser now</a></p>
+        <h2>Your are welcome to our Server</h2>
+    </div>
+    
+</body>
+</html>
