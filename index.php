cdcef702093cf1dbb8f83a89de7c9a022353bce7
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2024-08-09 13:56:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,23 +12,56 @@
         }
 
         .home{
+            background-image: url('/uploaded_img/Wills.jpg');
             height: 100vh; 
             display:flex; 
           align-items:center;
+          justify-content: center;
           background: linear-gradient(to top,
           rgba(255, 255, 255, 0.8),
           rgba(255, 0, 0, 0.6),
           rgba(255, 255, 255, 0.2)
-          );
+          ),
+          url('./images/default-avatar.png') center center;
+          background-repeat: no-repeat;
+          flex-direction: column;
+          background-size: cover;
+          
         }
+
+        .home h2{
+            color: aliceblue;
+            font-weight: 800;
+            font-family: Arial, Helvetica, sans-serif;
+            text-shadow: 2px 5px 50px black;
+        }
+
+        .home p{
+            font-size: 20px;
+            background-color: black;
+            color: white;
+            padding: 10px;
+            border-radius: 7px;
+            margin: 10px;
+        }
+
+        .home a{
+            color: wheat;
+        }
+
+       
      </style>
 </head>
 <body>
     
     
     <div class="home">
+        <h2>Your are welcome to our Server.</h2>
         <p >don't have an account? <a href="register.php">regiser now</a></p>
-        <h2>Your are welcome to our Server</h2>
+
+        <div class="cool">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima pariatur culpa numquam quis similique quam inventore aliquam voluptatem unde assumenda.</p>
+        </div>
     </div>
     
 </body>
